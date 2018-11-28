118c5c84dd02c3846c2852e9910389500f5ee3d5
Anton Savill <notna888+git@gmail.com>
2018-11-28 21:45:07 +0800

    adding my first blog and setting stuff up - it's all pretty much default at the moment

---
+++ content/Hello-World.md
@@ -0,0 +1,14 @@
+Title: My First Post
+Date: 2018-11-28 20:50
+Category: Blog
+
+# Is this thing on?
+
+I guess if you're reading this, that's a yes.
+Just like everyone else on the planet who has a blog, here's an awkward and exceedingly short first post.
+
+My name is Anton Savill, at the time of writing this I am in my mid-twenties, married and work as a software engineer. This website will probably get promptly forgotten about like most of my other projects I've worked on over the years. One day I might make my 'side projects' folder public on github, but for now you'll just have to trust me when I say that.
+
+This particular server is running on a Vultr instance, I have VentaCP installed to manage the actual server side of things and I've set up Pelican to act as my static site generation.
+
+And yeah, that's about it for now.
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Anton Savill'
+SITENAME = "Anton's Website"
+SITEURL = 'anton.savill.id.au'
+
+PATH = 'content'
+
+TIMEZONE = 'Australia/Perth'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('My Old, now abandoned Website', 'http://notna888.beevomit.org/'),
+         ('Work', 'http://sustainableplatform.com/team'),
+         ('', '#'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/notna888'),
+          ('Github', 'https://github.com/notna888'),
+          ('', '#'),
+         )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
